a928e3c2715e4630249a18169930b5bfedbd9ea5
Mathieu <mathieu.carpentier@rakuten.com>
2021-02-15 13:35:00 -0500

    Initial commit

---
+++ src/coveo/core.clj
@@ -0,0 +1,6 @@
+(ns coveo.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/coveo/core_test.clj
@@ -0,0 +1,7 @@
+(ns coveo.core-test
+  (:require [clojure.test :refer :all]
+            [coveo.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
